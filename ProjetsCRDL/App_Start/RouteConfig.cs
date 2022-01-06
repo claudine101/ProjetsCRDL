7defbdfa2ba2a95c78e7cf340d1dc3fcc5bec23c
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-06 12:11:31 +0200

    configuration route par defaut dashboard login

--- ProjetsCRDL/App_Start/RouteConfig.cs
+++ ProjetsCRDL/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProjetsCRDL
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Dashboard", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
