3d957b73a3b90acc7ec7bea8c31cd23adb142898
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-06 09:50:20 +0200

    Regenerate a new model which replaces what exists

--- ProjetsCRDL/Models/Recoltes.Designer.cs
+++ ProjetsCRDL/Models/Recoltes.Designer.cs
@@ -1,6 +1,4 @@
-﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\HP ProBook\Desktop\ProjetsCRDL\ProjetsCRDL\Models\Recolte.edmx'. 
+﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\HP ProBook\Desktop\ProjetsCRDL\ProjetsCRDL\Models\Recoltes.edmx'. 
 // Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
 // Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
 // dans le concepteur.
-//
-//
